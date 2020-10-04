48689531c998ad28f9c3f763960fba9614e872af
OTolstoi <63903984+OTolstoi@users.noreply.github.com>
2020-10-04 10:35:22 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,29 +19,23 @@ testWebP(function (support) {
 });
 
 
-
-// Button-call-me 
-
+// Button-callme 
+// sticky-navbar
 const btnCallMe = document.querySelector('.callme');
-
 let stickyNavbar = document.querySelector('.header__row2').offsetTop;
 
 window.addEventListener('scroll', () => {
 
    btnCallMe.hidden = (pageYOffset < 200);
 
-
    let navbar = document.querySelector('.header__row2');
 
    if (window.pageYOffset >= stickyNavbar) {
-
       navbar.classList.add("sticky");
 
    } else {
-
       navbar.classList.remove("sticky");
    }
-
 
 });
 
@@ -52,44 +46,34 @@ window.addEventListener('scroll', () => {
 
 document.querySelector('.hdr-links__phones').addEventListener('click', () => {
    let elem = document.querySelector('.phones__morephones');
-
    let arrow1 = document.querySelector('.morephones__arrow1');
    let arrow2 = document.querySelector('.morephones__arrow2');
 
-
    elem.classList.toggle('phones--active');
-
    arrow1.classList.toggle('phones--active');
    arrow2.classList.toggle('phones--active');
-
-
 
 })
 
 
 // Button footer morephones
 
-
 document.querySelector('.ftr-contacts__phones').addEventListener('click', () => {
    let elem = document.querySelector('.ftr-morephones');
    let arrow1 = document.querySelector('.ftr-contacts-phones__arrow1');
    let arrow2 = document.querySelector('.ftr-contacts-phones__arrow2');
 
-
    elem.classList.toggle('phones--active');
    arrow1.classList.toggle('phones--active');
    arrow2.classList.toggle('phones--active');
 
-
-
 })
-
-
 
 
 window.addEventListener('click', (e) => {
 
-   if (document.querySelector('.phones__morephones').matches('.phones--active') && !e.target.closest('.hdr-links__phones')) {
+   if (document.querySelector('.phones__morephones').matches('.phones--active') &&
+      !e.target.closest('.hdr-links__phones')) {
 
       document.querySelector('.phones__morephones').classList.remove('phones--active');
       document.querySelector('.morephones__arrow1').classList.remove('phones--active');
@@ -97,7 +81,8 @@ window.addEventListener('click', (e) => {
 
    }
 
-   if (document.querySelector('.ftr-morephones').matches('.phones--active') && !e.target.closest('.ftr-contacts__phones')) {
+   if (document.querySelector('.ftr-morephones').matches('.phones--active') &&
+      !e.target.closest('.ftr-contacts__phones')) {
 
       document.querySelector('.ftr-morephones').classList.remove('phones--active');
       document.querySelector('.ftr-contacts-phones__arrow1').classList.remove('phones--active');
@@ -108,7 +93,7 @@ window.addEventListener('click', (e) => {
 })
 
 
-// Button Search
+// Buttons Search
 
 document.querySelector('.navbar-search').addEventListener('click', () => {
 
@@ -119,10 +104,8 @@ document.querySelector('.navbar-search').addEventListener('click', () => {
       document.querySelector('body').classList.remove('lock');
    }
 
-
    document.querySelector('.navbar-search--hide').classList.toggle('search--active');
    document.querySelector('.navbar-search__block').classList.toggle('search--active');
-
    document.querySelector('.navbar-search__close').classList.toggle('search--active');
 
 })
@@ -139,6 +122,14 @@ document.querySelector('.navbar__link-searсh').addEventListener('click', (e) =>
 
 
 window.addEventListener('resize', () => {
+
+   if (document.documentElement.clientWidth > 991 &&
+      document.getElementById('menu').hasAttribute('style')) {
+
+      document.getElementById('menu').removeAttribute('style');
+   }
+
+
    if (document.querySelector('.navbar-search__block').matches('.search--active')) {
 
       document.querySelector('.navbar-search--hide').classList.add('search--active');
@@ -155,17 +146,40 @@ window.addEventListener('resize', () => {
 
    }
 
-   if (document.querySelector('.navbar__burger').matches('.menu-active') && document.documentElement.clientWidth > 991) {
+   let isMenuOpen = document.querySelector('.navbar__burger').matches('.menu-active');
 
+   if (isMenuOpen && document.documentElement.clientWidth > 991) {
+
+      document.getElementById('menu').removeAttribute('style');
       document.querySelector('.navbar__burger').classList.remove('menu-active');
       document.querySelector('.navbar__list').classList.remove('menu-active');
+      document.querySelector('.navbar__list').classList.remove('scroll');
       document.querySelector('body').classList.remove('lock');
 
+   } else if (isMenuOpen &&
+      document.querySelector('.navbar__list').matches('.scroll') &&
+      document.documentElement.clientHeight > 416) {
+
+      document.querySelector('.navbar__list').classList.remove('scroll');
+
+   } else if (isMenuOpen &&
+      document.getElementById('menu').hasAttribute('style') &&
+      document.documentElement.clientHeight > (417 + document.querySelector('.header__row2').getBoundingClientRect().top)) {
+
+      document.getElementById('menu').removeAttribute('style');
+
+   } else if (isMenuOpen &&
+      document.documentElement.clientHeight < (417 + document.querySelector('.header__row2').getBoundingClientRect().top)) {
+
+      let navbarTop = document.querySelector('.header__row2').getBoundingClientRect().top;
+      document.getElementById('menu').style.height = `calc(100vh - ${51 + navbarTop}px)`;
+
    }
-})
 
-//  Buttom BURGER
+});
 
+
+//  Button BURGER
 
 document.addEventListener("DOMContentLoaded", menuBurgerOpen);
 
@@ -183,11 +197,32 @@ function menuBurgerOpen() {
 
       }
 
+      let navbar = document.querySelector('.header__row2');
+      let navbarTop = navbar.getBoundingClientRect().top;
+
+      if (navbar.matches('.sticky') &&
+         document.documentElement.clientHeight < 417 &&
+         document.documentElement.clientWidth < 992) {
+
+         document.getElementById('menu').removeAttribute('style');
+         document.querySelector('.navbar__list').classList.add('scroll');
+
+      } else if (document.documentElement.clientHeight < (417 + navbarTop) &&
+         document.documentElement.clientWidth < 992) {
+
+         document.getElementById('menu').style.height = `calc(100vh - ${51 + navbarTop}px)`;
+
+      } else if (document.getElementById('menu').hasAttribute('style')) {
+
+         document.getElementById('menu').removeAttribute('style');
+
+      } else {
+         document.querySelector('.navbar__list').classList.remove('scroll');
+      }
+
       document.querySelector('.navbar__burger').classList.toggle('menu-active');
       document.querySelector('.navbar__list').classList.toggle('menu-active');
       document.querySelector('body').classList.toggle('lock');
 
-
-   });
-
+   })
 }
